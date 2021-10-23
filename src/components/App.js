f9d4c874a6da74ee2b0d7c5aa06bbe7cbb4cdf9a
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-10-23 23:27:06 +0500

    completed markup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import '../css/app.css';
+import '../css/main.css';
+import Trial from './Trial';
 
 function App() {
-  return null;
+  return (
+    <div className='container'>
+      <header id='header'>
+        <h1 className='header-title'>Simple, traffic-based pricing</h1>
+        <p className='header-description'>
+          Sign-up for our 30-day trial. No credit card required
+        </p>
+      </header>
+      <Trial />
+    </div>
+  );
 }
 
 export default App;
