19c74bafd51a75754f87f3fa072d4d3d2716cece
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-10-23 23:27:43 +0500

    created TrialInfo Component

---
+++ src/components/TrialInfo.js
@@ -0,0 +1,21 @@
+import iconCheck from '../images/icon-check.svg';
+
+function TrialInfo() {
+  return (
+    <div className='trial-info-grid'>
+      <ul className='trial-benefits'>
+        {BENEFITS.map((item, index) => (
+          <li key={index} className='trial-item'>
+            <img src={iconCheck} alt='Check Icon' />
+            <p className='trial-content'>{item}</p>
+          </li>
+        ))}
+      </ul>
+      <button className='btn'>Start my trial</button>
+    </div>
+  );
+}
+
+const BENEFITS = ['Unlimited websites', '100% data ownership', 'Email reports'];
+
+export default TrialInfo;
